146ad614e8abd322433b18736f6b5b207888ddcc
cqfnk <askakiyama@gmail.com>
2021-05-23 07:05:26 +0100

    set_dc vetorized

--- rpi/servo_test.py
+++ rpi/servo_test.py
@@ -8,29 +8,28 @@ FS90R = {
 }
 
 # 送信用のduty cycle パラメータに変換
-def to_dc(dcr,factor=10000.0):
+def to_pgdc(dcr,factor=10000.0):
     return int(dcr * factor)
-
-freq  = FS90R['freq']
-halt = to_dc(FS90R['halt'])
 
 pi = pigpio.pi()
 
-def set_dc(bcm,freq,dc):
-    pi.hardware_PWM(bcm,freq,halt+dc)
+def set_dc(bcms=FS90R['bcm_pin'].values(),
+           freq=FS90R['freq'],
+           dc=0):
+    for bcm in [bcms] if isinstance(bcms,int) else bcms:
+        pi.hardware_PWM(bcm,freq,to_pgdc(FS90R['halt']+dc))
 
 def init_FS90R():
     for wheel in ('R','L'):
         bcm = FS90R['bcm_pin'][wheel]
         pi.set_mode(bcm,pigpio.OUTPUT)
-        set_dc(bcm,freq,0)
+        set_dc()
                     
 init_FS90R()
 
 from time import sleep
 
-set_dc(FS90R['bcm_pin']['R'], freq, to_dc(FS90R['drive']))
-set_dc(FS90R['bcm_pin']['L'], freq, to_dc(FS90R['drive']))
+set_dc(dc=FS90R['drive'])
 sleep(1)
-set_dc(FS90R['bcm_pin']['R'], freq, 0)
-set_dc(FS90R['bcm_pin']['L'], freq, 0)
+set_dc()
+
