37e44b3f314f79cddc47998d379e55d79305c99d
cqfnk <askakiyama@gmail.com>
2021-05-23 09:40:34 +0100

    YDLIDAR X4 oneshot

--- rpi/oneshot.py
+++ rpi/oneshot.py
@@ -5,13 +5,10 @@ obj = PyLidar3.YdLidarX4(port)
 obj.Connect()
 
 gen=obj.StartScanning()
+
 data = next(gen)
-obj.StopScanning()
-
-obj.Disconnect()
-
 for angle,distance in data.items():
     print(angle,distance)
-            
 
-
+obj.StopScanning()
+obj.Disconnect()
