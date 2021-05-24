c30158dcb4c770638397c071b22a5326cb82cb96
cqfnk <askakiyama@gmail.com>
2021-05-24 11:41:29 +0100

    ydlidar 2D<->1D data

--- rpi/ydlidar_send.py
+++ rpi/ydlidar_send.py
@@ -1,6 +1,7 @@
 import PyLidar3
 port = '/dev/ttyUSB0'
-obj = PyLidar3.YdLidarX4(port) 
+chunk_size = 12000
+obj = PyLidar3.YdLidarX4(port,chunk_size=chunk_size) 
 
 import rclpy
 from std_msgs.msg import Int32MultiArray
@@ -16,7 +17,7 @@ gen=obj.StartScanning()
 try:
     while True:
         ydata = next(gen)
-        data = Int32MultiArray(data=[ y[0] * 65536 + y[1] for y in ydata.items() ])
+        data = Int32MultiArray(data=sum(ydata.items(),()))
         pub.publish(data)
         
 except Exception as e:
